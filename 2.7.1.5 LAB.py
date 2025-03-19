f14cb94a4cda513858306cfb90a1d0e2c18d2bfe
mbaars99 <mbaars99@gmail.com>
2025-03-19 12:55:43 +0100

    Update 2.7.1.5 LAB.py

--- 2.7.1.5 LAB.py
+++ 2.7.1.5 LAB.py
@@ -15,9 +15,15 @@ class bankaccount():
         return self.__accountnumber
     
     @rekening.setter
+    def rekening(self, newnumber):
+        print(f"Error: Changing bank details from {self.__accountnumber} to {newnumber} is not permitted")
+        return self.__accountnumber
+    
+    @rekening.deleter
     def rekening(self):
-        raise bankerror("Error: Changing bank details is not permitted")    
-
+        print(f"Error: Removing {self.__accountnumber} to {newnumber} is not permitted")
+        return self.__accountnumber
+    
     @property
     def account(self):
         return self.__accountbalance
@@ -28,18 +34,20 @@ class bankaccount():
         #     raise bankerror("Error: Not authorised to change account number: " + number)
         
         if self.__accountbalance + amount < 0:
-            raise bankerror("Error: Negative bank balance is not permitted for: " + self.__accountnumber)
+            raise bankerror("Negative bank balance is not permitted")
         
         if self.__accountbalance + amount > UPPER_AUDIT_AMOUNT:
-            print('Audit: Bankoperation exceeds 100.000€', end=' ')
+            print('Audit: Bankoperation exceeds 100.000€')
         
         self.__accountbalance += amount
         
     @account.deleter
-    def account(self, number):
-        if self.__accountbalance != 0:
-            raise bankerror("Error: Cannot delete account number: " + number + ", because balance is not zero.")
-
+    def account(self):
+        if self.__accountbalance == 0:
+            print(f"Account: {self.__accountnumber} has been deleted")
+        else:
+            raise bankerror(f"Cannot delete account number: {self.__accountnumber}, because the balance ({self.__accountbalance}€) is not zero.")
+            
 # test your class behavior by:
 # setting the balance to 1000;
 # trying to set the balance to -200;
@@ -48,23 +56,33 @@ class bankaccount():
 # trying to delete the account attribute containing a non-zero balance.
 
 knab = bankaccount()   
-print(f"Account number and balance: {knab.account}")             
+print(f"Account number and initial balance: {knab.account}")    
 
+print('\n# setting the balance to 1000')
 knab.account = 1000
+print(f"Balance: {knab.account}")   
+
+try:
+    print('\n# trying to set the balance to -200 (by subtracting -1200)')
+    knab.account = -1200
+    print(f"Account number and balance: {knab.account}")             
+except bankerror as e:
+    print("Error:", e)
+
+print("\n# trying to set a new value for the account number")
+knab.rekening = 'GE681232973485'
+
+print('\n# trying to deposit 1.000.000')
+knab.account += 1000000
 print(f"Account number and balance: {knab.account}")             
 
-knab.account = -200
-print(f"Account number and balance: {knab.account}")             
+try:
+    print('\n# trying to delete the account attribute containing a non-zero balance.')
+    del knab.account
+except bankerror as e:
+    print("Error:", e)
 
-knab.rekening = 'BE91798829733676'
-
-
-
-knab.account += 100000
-print(f"Account number and balance: {knab.account}")             
-
-
-
+print()
 
 
 
